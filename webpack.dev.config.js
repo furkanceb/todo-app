1e832e3d9a8b387cb7370abc820e1e2b385335b2
furkanceb <117816558+furkanceb@users.noreply.github.com>
2024-04-07 12:00:14 +0300

    add asset loading rules

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -19,7 +19,12 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+    
   },
   mode: 'development'
 };
